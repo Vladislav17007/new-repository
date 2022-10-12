7a5368ef0d9709e5d424546c90dba89dd8775a59
Владислав <56509864+Vladislav17007@users.noreply.github.com>
2022-10-13 01:20:38 +0500

    Added new pagination file Decomposition this code Added new features & actions to the App

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import { useFetching } from "./hooks/useFetching";
 import { getPageCount, getPagesArray } from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 
 function App() {
@@ -26,7 +27,6 @@ function App() {
   const [limit, setLimit] = useState(10)
   const [page, setPage] = useState(1)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  let pagesArray = getPagesArray(totalPages);
 
   const [fetchPosts, isPostsLoading, postError] = useFetching( async (limit, page) => {
     const response = await PostService.getAll(limit, page);
@@ -85,17 +85,11 @@ function App() {
             :<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
 
         }
-        <div className="page__wrapper">
-            {pagesArray.map(p =>
-                <span 
-                onClick={() => changePage(p)}
-                key={p} 
-                className={page === p ? 'page page__current' : 'page'}
-                >
-                      {p}
-                  </span>
-            )}
-        </div>
+        <Pagination 
+          page={page} 
+          changePage={changePage} 
+          totalPages={totalPages}
+        />        
       </div>
   );
 }
